7504a6834e27d60c6fe018fb1223dbb424a1041d
leodeth <59341677+leodeth@users.noreply.github.com>
2019-12-31 15:52:38 -0400

    Buscar valor unico array
    Buscar valor unico dentro de un array

---
+++ ArrayValorUnico.php
@@ -0,0 +1,27 @@
+<?php
+
+//buscar elemento unico en array
+//array_count_values solo funciona con numeros enteros y cadenas
+
+$array1 = [ 1, 2, 1, 1, 1, 1 ];
+$array2 = [ 0, 0, 8, 0, 0,0 ];
+
+$counts = array_count_values($array1);  // since php4
+print_r($counts);
+foreach ($counts as $value => $count) {
+    if ($count == 1) {
+        $uniques[] = $value;
+    } else {
+        $duplicates[] = $value . "<br>";
+    }
+}
+
+//implode: Join array elements with a string
+
+
+//echo "unique values: " , implode(", ", $uniques) , "\n";
+//echo "duplicate values: " , implode(", ", $duplicates);
+
+
+echo "unique values: " , implode($uniques) , "\n";
+echo "duplicate values: " , implode($duplicates);
